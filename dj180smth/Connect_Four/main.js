04f7c25c0f67a71d7c8bd267b3ff7a24b7030e7d
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-07-27 02:18:08 +0530

    nested loop removed, trying ot regex

--- dj180smth/Connect_Four/main.js
+++ dj180smth/Connect_Four/main.js
@@ -4,3 +4,12 @@ const B = "A"
 // const B = window.prompt("Enter Player Two's Name:")
 
 $('#player_id').css('color','pink')
+
+$('div.row1>i').on('click',function(){
+    console.log(this);
+    let liss = $(/row\d/)
+    console.log(liss)
+    for(let i=liss.length;i>0;i--){
+        console.log(liss.eq(i))
+    }
+})
